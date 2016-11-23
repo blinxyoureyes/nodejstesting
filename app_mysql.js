1d89b3bc8cdef76971000ca0da06c00ad8a894a2
stu <blinxyoureyes@gmail.com>
2016-11-23 17:07:07 +0700

    updt

--- app_mysql.js
+++ app_mysql.js
@@ -1,19 +1,36 @@
-var conf = require("./config.js");
-var conn = require("./db.js");
+var http = require('http'),
+    qs   = require('querystring'),
+    conf = require("./config.js");
+    // conn = require("./db.js");
 
 conf.writeLog("start query : "+conf.showtime);
 
-var query = 'SELECT * FROM tbl_user';
+http.createServer(function(req, res) {
+    if (req.method === 'POST' && req.url === '/login') {
+        var body = '';
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+        req.on('end', function() {
+            var data = qs.parse(body);
+            // now you can access `data.email` and `data.password`
+            res.writeHead(200);
+            res.end(JSON.stringify(data));
+        });
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+}).listen(8000);
 
-var r = conn.dataquery(query, function(result) {
-    var arrpush = [];
-    result.forEach(function (hasil) {
-        var arrGet = 'Data result : ' + hasil.username;
-        arrpush.push(arrGet);
-        console.log(arrGet);
-    });
-    console.log(arrpush);
-    conf.writeLog("end query : "+arrpush+" "+conf.showtime);
-});
-
-
+// var query = 'SELECT * FROM tbl_user';
+// var r = conn.dataquery(query, function(result) {
+//     var arrpush = [];
+//     result.forEach(function (hasil) {
+//         var arrGet = 'Data result : ' + hasil.username;
+//         arrpush.push(arrGet);
+//         console.log(arrGet);
+//     });
+//     console.log(arrpush);
+//     conf.writeLog("end query : "+arrpush+" "+conf.showtime);
+// });
