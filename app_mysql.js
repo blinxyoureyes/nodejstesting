8c71855684dc678b6e8f313dd55c423275ebcf3f
stu <blinxyoureyes@gmail.com>
2016-11-24 11:50:55 +0700

    updt

--- app_mysql.js
+++ app_mysql.js
@@ -5,32 +5,14 @@ var http = require('http'),
 
 conf.writeLog("start query : "+conf.showtime);
 
-http.createServer(function(req, res) {
-    if (req.method === 'POST' && req.url === '/login') {
-        var body = '';
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-        req.on('end', function() {
-            var data = qs.parse(body);
-            // now you can access `data.email` and `data.password`
-            res.writeHead(200);
-            res.end(JSON.stringify(data));
-        });
-    } else {
-        res.writeHead(404);
-        res.end();
-    }
-}).listen(8000);
-
-// var query = 'SELECT * FROM tbl_user';
-// var r = conn.dataquery(query, function(result) {
-//     var arrpush = [];
-//     result.forEach(function (hasil) {
-//         var arrGet = 'Data result : ' + hasil.username;
-//         arrpush.push(arrGet);
-//         console.log(arrGet);
-//     });
-//     console.log(arrpush);
-//     conf.writeLog("end query : "+arrpush+" "+conf.showtime);
-// });
+var query = 'SELECT * FROM tbl_user';
+var r = conn.dataquery(query, function(result) {
+    var arrpush = [];
+    result.forEach(function (hasil) {
+        var arrGet = 'Data result : ' + hasil.username;
+        arrpush.push(arrGet);
+        console.log(arrGet);
+    });
+    console.log(arrpush);
+    conf.writeLog("end query : "+arrpush+" "+conf.showtime);
+});
--- server2.js
+++ server2.js
@@ -3,11 +3,34 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
+// include another
+var http = require('http'),
+    // qs   = require('querystring'),
+    conn = require("./db.js"),
+    conf = require("./config.js");
+
 // routes will go here
 app.get('/', function(req, res) {
     var username = req.param('username');
     var password = req.param('password');
-    res.send('username='+username+'&password='+password);
+    var appndParams = 'username='+username+' - password='+password;
+    conf.writeLog("["+conf.showtime+"] Param Request : "+appndParams);
+
+    // connect & select to database
+    var query = 'SELECT * FROM tbl_user';
+    var r = conn.dataquery(query, function(result) {
+        var arrpush = [];
+        result.forEach(function (hasil) {
+            var arrGet = 'Data result : ' + hasil.username;
+            arrpush.push(arrGet);
+            console.log(arrGet);
+        });
+        console.log(arrpush);
+        conf.writeLog("end query : "+arrpush+" "+conf.showtime);
+    });
+
+    // print result
+    res.send('username='+username+'\npassword='+password);
 });
 
 // start the server
